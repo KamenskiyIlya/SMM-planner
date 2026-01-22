0bc2324fe626b312cd88a2c7c00245b87af94d7b
KamenskiyIlya <kamenskj.ilya@gmail.com>
2026-01-22 21:54:20 +0300

    Удалил блок main() из tg_publisher

--- tg_publisher.py
+++ tg_publisher.py
@@ -10,22 +10,3 @@ def upload_post(text: str, photo, chat_id, bot):
 		return False
 
 	# доделать отработку, если пользователь передает только текст или только фото
-
-
-def main():
-	env = Env()
-	env.read_env()
-	chat_id = env.str('CHAT_ID')
-	bot = telegram.Bot(token=env.str('TG_BOT_TOKEN'))
-
-	# ссылка на фото для примера, позже настроим постинг в нужном формате
-	upload_post(
-		'Привет!',
-		'https://www.litres.ru/journal/c/covers/212185/logo/212185_logo.jpg',
-		chat_id,
-		bot
-	)
-
-
-if __name__ == '__main__':
-	main()

fd2ea2be8536d8b3cac298ab951d499727ee815f
KamenskiyIlya <kamenskj.ilya@gmail.com>
2026-01-23 20:55:03 +0300

    Расширил обработку потенциальных ошибок у tg_publisher

--- tg_publisher.py
+++ tg_publisher.py
@@ -4,28 +4,32 @@ from environs import Env
 
 
 def publish_post_to_tg(text: str, photo = None):
-	env = Env()
-	env.read_env()
-	chat_id = env.str('CHAT_ID')
-	bot = telegram.Bot(token=env.str('TG_BOT_TOKEN'))
-	if photo:
-		result = bot.send_photo(chat_id=chat_id, photo=photo, caption=text)
-		return result['message_id']
-	if not photo:
-		result = bot.send_message(chat_id=chat_id, text=text)
-		return result['message_id']
+	try:
+		env = Env()
+		env.read_env()
+		chat_id = env.str('CHAT_ID')
+		bot = telegram.Bot(token=env.str('TG_BOT_TOKEN'))
+		if photo:
+			result = bot.send_photo(chat_id=chat_id, photo=photo, caption=text)
+			return result['message_id']
+		if not photo:
+			result = bot.send_message(chat_id=chat_id, text=text)
+			return result['message_id']
+	except Exception as er:
+		pass
 
 
 def delete_post_from_tg(post_id):
-	env = Env()
-	env.read_env()
-	chat_id = env.str('CHAT_ID')
-	bot = telegram.Bot(token=env.str('TG_BOT_TOKEN'))
 	try:
+		env = Env()
+		env.read_env()
+		chat_id = env.str('CHAT_ID')
+		bot = telegram.Bot(token=env.str('TG_BOT_TOKEN'))
+
 		result = bot.delete_message(chat_id=chat_id, message_id=post_id)
 		return result
-	except telegram.error.BadRequest as er:
-		print(er)
+	except Exception as er:
+		pass
 		
 
 	# доделать отработку, если пользователь передает только текст или только фото
